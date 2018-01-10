040f771bc9509e72f32b6af25363010f2c992b14
Brian Bixby <brianbixby0@gmail.com>
2018-01-10 11:32:21 -0800

    all data displays and sorted; needs style

--- brian-ken/scripts/article.js
+++ brian-ken/scripts/article.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('article works');
-
 let articles = [];
 
 // COMMENT: What is the purpose of the following function? Why is its name capitalized? Explain the context of "this" within the function. What does "rawDataObj" represent?
@@ -11,11 +9,10 @@ function Article(rawDataObj) {
   // TODO: Use the JS object that is passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
   this.rawDataObj=rawDataObj;
-  console.log(rawDataObj);
 }
 
 Article.prototype.toHtml = function() {
-  console.log('18');
+  console.log('18', this.rawDataObj.author);
   // COMMENT: What is the benefit of cloning the article? (see the jQuery docs)
   // PUT YOUR RESPONSE HERE
   // Allows programmers to move elements without removing or deleting the first instance of the element. Also, you can modify cloned elements before resubmitting them into the document.
@@ -26,6 +23,7 @@ Article.prototype.toHtml = function() {
   $newArticle.addClass('cloned');
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
+  $newArticle.attr('data-authorUrl', this.authorUrl);
 
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
@@ -34,18 +32,18 @@ Article.prototype.toHtml = function() {
       3. article title,
       4. article body, and
       5. publication date. */
-  $newArticle.attr('data-author', this.author);
-  $newArticle.attr('data-authorUrl', this.authorUrl);
-  $newArticle.attr('data-title', this.title);
-  $newArticle.attr('data-body', this.body);
-  $newArticle.attr('data-publishedOn', this.publishedOn);
+  // $newArticle.attr('data-publishedOn', this.publishedOn);
+
+  $newArticle.find('h1').html(this.rawDataObj.title);
+  $newArticle.find('a').html(this.rawDataObj.author);
+  $newArticle.find('.article-body').html(this.rawDataObj.body);
+  $newArticle.find('a').attr('href', this.rawDataObj.authorUrl);
 
   // REVIEW: Display the date as a relative number of 'days ago'
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.rawDataObj.publishedOn))/60/60/24/1000) + ' days ago');
   $newArticle.append('<hr>');
   console.log('46', $newArticle[0].innerHTML);
   return $newArticle[0].innerHTML;
-  
 };
 
 rawData.sort(function(a,b) {
