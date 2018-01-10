321fd71b1bc403b3e0a22448b5796745662e2ec5
nicoleweese <nweese@ymail.com>
2018-01-10 10:15:22 -0800

    did some js work

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -23,10 +23,17 @@ Article.prototype.toHtml = function() {
 
   let $newArticle = $('article.template').clone();
   /* TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
+  $newArticle.removeClass('template');
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
+  $newArticle.find('address').html('<a href="' + this.authorUrl + '">' + this.author + '</a>');
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('.article-body').html('<p>' + this.body + '</p>');
+  $newArticle.find('time').html(this.publishedOn);
+  
+  
   /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
       1. author name,
@@ -41,16 +48,16 @@ Article.prototype.toHtml = function() {
   return $newArticle;
 };
 
-rawData.sort(function(a,b) {
-  // REVIEW: Take a look at this sort method; This may be the first time we've seen it. Look at the docs and think about how the dates would be sorted if the callback were not included in this method.
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
+// rawData.sort(function(a,b) {
+//   // REVIEW: Take a look at this sort method; This may be the first time we've seen it. Look at the docs and think about how the dates would be sorted if the callback were not included in this method.
+//   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+// });
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
-}
+// for(let i = 0; i < rawData.length; i++) {
+//   articles.push(new Article(rawData[i]));
+// }
 
 for(let i = 0; i < articles.length; i++) {
   $('#articles').append(articles[i].toHtml());
